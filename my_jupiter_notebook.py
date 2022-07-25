af3e86a2ec5cf956409569480c6af855a4bf7688
Thigshicca Vigneshwaramoorthy <104121594+THIGSHICCA@users.noreply.github.com>
2022-07-25 21:50:07 +0530

    Add files via upload
    First commit

---
+++ my_jupiter_notebook.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def func_name(text):
+    return ''.join([letter * 3 for letter in text])
+
+print(func_name('hello'))
+
+
+# In[20]:
+
+
+str = "I am home"
+
+words = str.split()
+
+words = list(reversed(words))
+
+
+func_name = ('I am home')
+def reverse(func_name):
+    s = func_name.split()
+    return ' '.join(s[::-1])
+reverse(func_name)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
